fb565f33c01468bfd0634101d1a6e124928dfb96
Reactongraph <js.developer@ongraph.com>
2022-09-01 18:34:47 +0530

    class component to functon component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AppNavigator from './Container';
-import { Root } from 'native-base';
+import {Root} from 'native-base';
 
-const App: () => React$Node = () => {
-	return (
-		<Root>
-			<AppNavigator />
-		</Root>
-	);
+const App = () => {
+  return (
+    <Root>
+      <AppNavigator />
+    </Root>
+  );
 };
 
 export default App;
